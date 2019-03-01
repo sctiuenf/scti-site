25f993bf871d7f625e77cad01490daf66b2df832
Pedro <pedrohb88@hotmail.com>
2019-03-01 18:52:20 -0300

    Add basic structure

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SCTI 2019</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <h1>Coming soon.</h1>
+
+
+
+        <script src="js/index.js"></script>
+    </body>
+</html>
