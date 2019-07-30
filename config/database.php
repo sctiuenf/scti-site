271838987476d7c531b11e5255251c27cd7bd261
Pedro <pedrohb88@hotmail.com>
2019-07-30 01:21:39 -0300

    db connection heroku

--- config/database.php
+++ config/database.php
@@ -1,7 +1,17 @@
 <?php 
-    $DB_HOST = getenv('DB_HOST');
-    $DB_NAME = getenv('DB_NAME');
-    $DB_USERNAME = getenv('DB_USERNAME');
-    $DB_PASSWORD = getenv('DB_PASSWORD');
-    $DB_CHARSET = 'utf8';
+   
+   if(strlen(getenv('DB_HOST') > 0)){
+        $DB_HOST = getenv('DB_HOST');
+        $DB_NAME = getenv('DB_NAME');
+        $DB_USERNAME = getenv('DB_USERNAME');
+        $DB_PASSWORD = getenv('DB_PASSWORD');
+        $DB_CHARSET = 'utf8';
+   }else{
+        $url = parse_url(getenv('CLEARDB_DATABASE_URL'));
+        $DB_HOST = $url['host'];
+        $DB_NAME = substr($url['path'], 1);
+        $DB_USERNAME = $url['user'];
+        $DB_PASSWORD = $url['pass'];
+        $DB_CHARSET = 'utf8';
+   }  
 ?>
